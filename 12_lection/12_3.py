bcefeac35fa7604c6e22310285f1ea33c2e238a4
KhakhinaAA <ana-khakhina@yandex.ru>
2026-01-25 15:03:47 +0300

    закончено задание 12_3

--- 12_lection/12_3.py
+++ 12_lection/12_3.py
@@ -3,3 +3,62 @@
 # Убедитесь, что в заголовке вкладки отображается "Задача №НОМЕР от ДАТА",
 # где ДАТА и НОМЕР - это ваши эталонные значения
 # Проверьте, что поля: Исполнитель, дата, номер, описание и автор отображаются с эталонными значениями
+
+from atf.ui import *
+from atf import *
+from time import sleep
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+
+excecutor_txt = 'Хахина А.А.'
+date_txt = '25 янв, вс'
+number_txt = '879'
+descrub_txt = 'Купить всем игристого'
+
+
+
+class AuthOnline(Region):
+    login_inp = TextField(By.CSS_SELECTOR,'.controls-InputBase__field.controls-InputBase__field_margin-null.controls-InputBase__field_theme_default_margin-null.controls-Render__field.controls-Render__field_textAlign_left.ws-ellipsis.controls-Render__field_zIndex>[inputmode="text"]', 'логин')
+    password_inp = TextField(By.CSS_SELECTOR, '.controls-Field.js-controls-Field.controls-InputBase__nativeField.controls-Password__nativeField_caretFilled.controls-Password__nativeField_caretFilled_theme_default.controls-InputBase__nativeField_hideCustomPlaceholder', 'пароль')
+
+class TaskOpt(Region):
+    executor = Element(By.CSS_SELECTOR, '[data-qa="SelectedCollection__item__caption"]', 'Исполнитель')
+    date = Element(By.CSS_SELECTOR, ' [data-qa="edo3-Document_docDate"] [class="controls-EditableArea__Text__inner"]', 'Дата')
+    number = Element(By.CSS_SELECTOR, '[data-qa="edo3-Document_docNumber"] [class="controls-EditableArea__Text__inner"]', 'Номер')
+    descrubtion = Element(By.CSS_SELECTOR, ' [data-qa="mte"] [class="mte-formattedText__part text-style"]', 'Описание')
+    author = Element(By.CSS_SELECTOR, '[data-qa="edo3-Sticker__mainInfo"][title="Хахина А.А."]', 'Автор')
+
+
+# class UrlTask(Region):
+#     url_task = Link(By.LINK_TEXT, 'https://fix-online.sbis.ru/doc/95af7896-630f-4e68-b9ca-ae8f1b1b7ef8?client=6242047', 'ссылка на задачу')
+
+class Test(TestCaseUI):
+    def test(self):
+        sbis_site = self.config.get('SBIS_SITE')
+        self.browser.open(sbis_site)
+
+        log('Авторизоваться')
+        user_login, user_password = 'khakhinaNastya', 'khakhinaNastya123'
+        auth = AuthOnline(self.driver)
+        auth.login_inp.type_in(user_login + Keys.ENTER).should_be(ExactText(user_login))
+        auth.password_inp.type_in(user_password + Keys.ENTER).should_be(Not(Visible))
+
+        log('Переход по прямой ссылке')
+        url_task_txt = 'https://fix-online.sbis.ru/doc/95af7896-630f-4e68-b9ca-ae8f1b1b7ef8?client=6242047'
+        self.browser.create_new_tab(url_task_txt)
+        self.browser.switch_to_opened_window()
+
+
+        log('Проверка заголовка')
+        sbis_title = 'Задача №879 от 25.01.26'
+        self.browser.should_be(TitleExact(sbis_title))
+
+
+        log('Проверка исполнителя, даты, номера, описания, автора')
+        txt = TaskOpt(self.driver)
+        txt.date.should_be(ExactText(date_txt))
+        txt.executor.should_be(ExactText(excecutor_txt))
+        txt.number.should_be(ExactText(number_txt))
+        txt.descrubtion.should_be(ExactText(descrub_txt))
+        txt.author.should_be(ExactText(excecutor_txt))
+
